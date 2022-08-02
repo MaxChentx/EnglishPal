8cb34e56ba752bab2a18d1f33a3beb2ee5e74363
Lan Hui <1348141770@qq.com>
2022-08-02 12:26:18 +0800

    Refactor: remove duplicate code block

--- app/static/js/highlight.js
+++ app/static/js/highlight.js
@@ -25,23 +25,13 @@ function highLight() {
     let articleContent = document.getElementById("article").innerText;
     let pickedWords = document.getElementById("selected-words");  // words picked to the text area
     let dictionaryWords = document.getElementById("selected-words2"); // words appearing in the user's new words list
-    if (pickedWords != null) {
-        const list = pickedWords.value.split(" ");
-        for (let i = 0; i < list.length; ++i) {
-            list[i] = list[i].replace(/(^\s*)|(\s*$)/g, ""); //消除单词两边的空字符
-            if (list[i] !== "" && "<mark>".indexOf(list[i]) === -1 && "</mark>".indexOf(list[i]) === -1) {
-		//将文章中所有出现该单词word的地方改为：" <mark>" + word + "<mark> "。 正则表达式RegExp()中，"\\s"代表单词前后必须要有空格，以防止只对单词中的部分字符高亮的情况出现。
-                articleContent = articleContent.replace(new RegExp("\\s"+list[i]+"\\s", "g"), " <mark>" + list[i] + "</mark> ");
-            }
-        }
-    }
-    if (dictionaryWords != null) {
-        const list2 = dictionaryWords.value.split(" ");
-        for (let i = 0; i < list2.length; ++i) {
-            list2[i] = list2[i].replace(/(^\s*)|(\s*$)/g, "");
-            if (list2[i] !== "" && "<mark>".indexOf(list2[i]) === -1 && "</mark>".indexOf(list2[i]) === -1) {
-                articleContent = articleContent.replace(new RegExp("\\s"+list2[i]+"\\s", "g"), " <mark>" + list2[i] + "</mark> ");
-            }
+    let allWords = pickedWords.value + " " + dictionaryWords.value;
+    const list = allWords.split(" ");
+    for (let i = 0; i < list.length; ++i) {
+        list[i] = list[i].replace(/(^\s*)|(\s*$)/g, ""); //消除单词两边的空字符
+        if (list[i] !== "" && "<mark>".indexOf(list[i]) === -1 && "</mark>".indexOf(list[i]) === -1) {
+	    //将文章中所有出现该单词word的地方改为：" <mark>" + word + "<mark> "。 正则表达式RegExp()中，"\\s"代表单词前后必须要有空格，以防止只对单词中的部分字符高亮的情况出现。
+            articleContent = articleContent.replace(new RegExp("\\s"+list[i]+"\\s", "g"), " <mark>" + list[i] + "</mark> ");
         }
     }
     document.getElementById("article").innerHTML = articleContent;
