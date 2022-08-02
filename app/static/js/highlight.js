47e745e7742658c10c4b201b92719e690fd35b27
Lan Hui <1348141770@qq.com>
2022-08-02 11:45:21 +0800

    Use better variable name (use articleContent instead of txt, and use camelCase)

--- app/static/js/highlight.js
+++ app/static/js/highlight.js
@@ -22,56 +22,56 @@ function getWord() {
 
 function highLight() {
     if (!isHighlight) return;
-    let txt = document.getElementById("article").innerText;
-    let picked_words = document.getElementById("selected-words");
-    let dictionary_words = document.getElementById("selected-words2");
-    if (picked_words != null) {
-        const list = picked_words.value.split(" ");
+    let articleContent = document.getElementById("article").innerText;
+    let pickedWords = document.getElementById("selected-words");  // words picked to the text area
+    let dictionaryWords = document.getElementById("selected-words2"); // words appearing in the user's new words list
+    if (pickedWords != null) {
+        const list = pickedWords.value.split(" ");
         for (let i = 0; i < list.length; ++i) {
             list[i] = list[i].replace(/(^\s*)|(\s*$)/g, ""); //消除字符串两边空字符
             if (list[i] !== "" && "<mark>".indexOf(list[i]) === -1 && "</mark>".indexOf(list[i]) === -1) {
                 //将正则表达式进行修改 表示搜索的是两端带有空格的list[i] 那么搜索的便为一个完整单词  然后在mark标签的前后各加了一个空格对其进行替换
-                txt = txt.replace(new RegExp("\\s"+list[i]+"\\s", "g"), " <mark>" + list[i] + "</mark> ");
+                articleContent = articleContent.replace(new RegExp("\\s"+list[i]+"\\s", "g"), " <mark>" + list[i] + "</mark> ");
             }
         }
     }
-    if (dictionary_words != null) {
-        const list2 = dictionary_words.value.split(" ");
+    if (dictionaryWords != null) {
+        const list2 = dictionaryWords.value.split(" ");
         for (let i = 0; i < list2.length; ++i) {
             list2[i] = list2[i].replace(/(^\s*)|(\s*$)/g, "");
             if (list2[i] !== "" && "<mark>".indexOf(list2[i]) === -1 && "</mark>".indexOf(list2[i]) === -1) {
                 //将正则表达式进行修改 表示搜索的是两端带有空格的list2[i] 那么搜索的便为一个完整单词  然后在mark标签的前后各加了一个空格对其进行替换
-                txt = txt.replace(new RegExp("\\s"+list2[i]+"\\s", "g"), " <mark>" + list2[i] + "</mark> ");
+                articleContent = articleContent.replace(new RegExp("\\s"+list2[i]+"\\s", "g"), " <mark>" + list2[i] + "</mark> ");
             }
         }
     }
-    document.getElementById("article").innerHTML = txt;
+    document.getElementById("article").innerHTML = articleContent;
 }
 
 function cancel_highLight() {
-    const list = picked_words.value.split(" ");
-    let txt = document.getElementById("article").innerText;
-    let picked_words = document.getElementById("selected-words");
-    const dictionary_words = document.getElementById("selected-words2");
-    if (picked_words != null) {
+    const list = pickedWords.value.split(" ");
+    let articleContent = document.getElementById("article").innerText;
+    let pickedWords = document.getElementById("selected-words");
+    const dictionaryWords = document.getElementById("selected-words2");
+    if (pickedWords != null) {
         for (let i = 0; i < list.length; ++i) {
             list[i] = list[i].replace(/(^\s*)|(\s*$)/g, "");
             if (list[i] !== "") {
-                txt = txt.replace("<mark>" + list[i] + "</mark>", "list[i]");
+                articleContent = articleContent.replace("<mark>" + list[i] + "</mark>", "list[i]");
             }
         }
     }
-    if (dictionary_words != null) {
-        let list2 = picked_words.value.split(" ");
+    if (dictionaryWords != null) {
+        let list2 = pickedWords.value.split(" ");
         for (let i = 0; i < list2.length; ++i) {
-            list2 = dictionary_words.value.split(" ");
+            list2 = dictionaryWords.value.split(" ");
             list2[i] = list2[i].replace(/(^\s*)|(\s*$)/g, "");
             if (list2[i] !== "") {
-                txt = txt.replace("<mark>" + list[i] + "</mark>", "list[i]");
+                articleContent = articleContent.replace("<mark>" + list[i] + "</mark>", "list[i]");
             }
         }
     }
-    document.getElementById("article").innerHTML = txt;
+    document.getElementById("article").innerHTML = articleContent;
 }
 
 function fillInWord() {
